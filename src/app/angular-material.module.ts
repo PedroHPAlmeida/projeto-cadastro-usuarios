8600193315ae78a6138fad4625a5a1679acc0602
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2024-09-12 18:43:43 -0300

    Adiciona campos de usuário e email

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from "@angular/core";
 import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
     imports: [
         MatCardModule,
+        MatInputModule,
+        MatFormFieldModule,
     ],
     exports: [
         MatCardModule,
+        MatInputModule,
+        MatFormFieldModule,
     ],
 })
 export class AngularMaterialModule { }
